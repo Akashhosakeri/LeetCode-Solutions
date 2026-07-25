674816e1c6c208b967536eb0bc84d14908be7542
Akashhosakeri <152246418+Akashhosakeri@users.noreply.github.com>
2026-07-25 10:41:11 +0530

    Time: 2 ms (92.08%), Space: 47.2 MB (98.88%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,7 +3,7 @@ class Solution {
         int n = nums.length;
         int j = -1;
         for(int i=0;i<n;i++){
-            if(nums[i]==0){
+            if(nums[i] == 0){
                 j = i;
                 break;
             }
@@ -11,9 +11,9 @@ class Solution {
         if(j == -1) return;
         for(int i=j+1;i<n;i++){
             if(nums[i]!=0){
-                int temp = nums[j];
-                nums[j] = nums[i];
-                nums[i] = temp;
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
                 j++;
             }
         }
