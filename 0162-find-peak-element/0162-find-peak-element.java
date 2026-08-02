e0a87cdd5039dce959c75432f1e9bee4c30d87e9
Akashhosakeri <152246418+Akashhosakeri@users.noreply.github.com>
2026-08-02 09:00:30 +0530

    Time: 0 ms (100%), Space: 44.3 MB (18.96%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -10,8 +10,6 @@ class Solution {
             int mid = (left+right)/2;
             if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
                 return mid;
-            }else if(nums[mid]>nums[mid-1]){
-                left = mid+1;
             }else if(nums[mid]>nums[mid+1]){
                 right = mid-1;
             }else{
