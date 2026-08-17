6e303ef5732669d7fe8e12e83d3d1c9f37ff4e0d
Akashhosakeri <152246418+Akashhosakeri@users.noreply.github.com>
2026-08-17 08:59:12 +0530

    Time: 0 ms (100%), Space: 42.1 MB (33.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,16 @@
 class Solution {
     public int fib(int n) {
-        if(n <= 1) return n;
+        if (n <= 1) return n;
 
-        int last = fib(n-1);
-        int slast = fib(n-2);
-        return last + slast;
+        int a = 0;
+        int b = 1;
+
+        for (int i = 2; i <= n; i++) {
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
     }
 }
